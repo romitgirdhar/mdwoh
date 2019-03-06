6530798c61bc38a15166fe06313da3c45921a5d0
Ryan CrawCour <38923605+ryancrawcour@users.noreply.github.com>
2019-03-06 15:18:53 -0800

    the mother of ALL notebooks!

--- notebooks/conformation_v2.py
+++ notebooks/conformation_v2.py
@@ -26,4 +26,111 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
-orders = spark.read.format("csv").options(inferSchema='true', header='true').load("/mnt/raw/orders")
+#Process all raw orders
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import uuid
+
+uuidUdf = udf(lambda : str(uuid.uuid4()), StringType())
+
+#load southridge orders
+sr_orders = spark.read.format("csv").options(inferSchema='true', header='true').load("/mnt/raw/orders")
+sr_orders = sr_orders.withColumn(
+  "ShipDateTemp", sr_orders.ShipDate.cast(TimestampType())).drop(
+  "ShipDate").withColumnRenamed(
+  "ShipDateTemp", "ShipDate").withColumn(
+  "OriginSystem", lit("southridge")).withColumn(
+  "SaleType", lit("sale"))
+  
+sr_orderdetails = spark.read.format("csv").options(inferSchema='true', header='true').load("/mnt/raw/order details")
+
+def toTimestamp(cName):
+ return from_unixtime(unix_timestamp(cName.cast("string"), "yyyyMMdd")).cast("timestamp")
+
+#load vander orders
+vander_txs = spark.read.format("csv").options(inferSchema='true', header='true').load("/mnt/raw/raw/vander/Transactions")
+
+vander_orders = vander_txs.select(
+  col("TransactionID").alias("OrderID"), 
+  col("CustomerID"), 
+  toTimestamp(col("RentalDate")).alias("OrderDate"),
+  (vander_txs.RentalCost + vander_txs.LateFee).alias("TotalCost"),
+  col("CreatedDate"), 
+  col("UpdatedDate"),
+  toTimestamp(col("RentalDate")).alias("ShipDate"))
+  
+vander_orders = vander_orders.withColumn("OriginSystem", lit("vander")).withColumn("SaleType", lit("rental"))
+
+vander_rentaldetails = vander_txs.select(
+  uuidUdf().alias("OrderDetailsID"), 
+  col("TransactionID").alias("OrderID"), "MovieID", 
+  lit(1).alias("Quantity"), 
+  col("RentalCost").cast(DoubleType()).alias("UnitCost"), 
+  lit(1).alias("LineNumber"), 
+  "CreatedDate", 
+  "UpdatedDate")
+
+vander_latedetails = vander_txs.filter(vander_txs.LateFee>0).select(
+  uuidUdf().alias("OrderDetailsID"), 
+  col("TransactionID").alias("OrderID"), 
+  "MovieID", 
+  lit(1).alias("Quantity"), 
+  col("LateFee").cast(DoubleType()).alias("UnitCost"), 
+  lit(2).alias("LineNumber"), 
+  "CreatedDate", 
+  "UpdatedDate")
+
+vander_orderdetails = vander_rentaldetails.union(vander_latedetails).orderBy("OrderID", "LineNumber")
+
+#load fourthcoffee orders
+fourth_txs = spark.read.format("csv").options(inferSchema='true', header='true').load("/mnt/raw/raw/four coffee/transactions")
+
+fourth_orders = fourth_txs.select(
+  col("TransactionID").alias("OrderID"), 
+  col("CustomerID"), 
+  toTimestamp(col("RentalDate")).alias("OrderDate"),
+  (fourth_txs.RentalCost + fourth_txs.LateFee).alias("TotalCost"),
+  col("CreatedDate"), 
+  col("UpdatedDate"), 
+  toTimestamp(col("RentalDate")).alias("ShipDate"))
+
+fourth_orders = fourth_orders.withColumn("OriginSystem", lit("fourth_coffee")).withColumn("SaleType", lit("rental"))
+
+fourth_rentaldetails = fourth_txs.select(
+  uuidUdf().alias("OrderDetailsID"), 
+  col("TransactionID").alias("OrderID"), 
+  "MovieID", 
+  lit(1).alias("Quantity"), 
+  col("RentalCost").cast(DoubleType()).alias("UnitCost"), 
+  lit(1).alias("LineNumber"), 
+  "CreatedDate", 
+  "UpdatedDate")
+
+fourth_latedetails = fourth_txs.filter(fourth_txs.LateFee>0).select(
+  uuidUdf().alias("OrderDetailsID"), 
+  col("TransactionID").alias("OrderID"), 
+  "MovieID", 
+  lit(1).alias("Quantity"), 
+  col("LateFee").cast(DoubleType()).alias("UnitCost"), 
+  lit(2).alias("LineNumber"), 
+  "CreatedDate", 
+  "UpdatedDate")
+
+fourth_orderdetails = fourth_rentaldetails.union(fourth_latedetails).orderBy("OrderID", "LineNumber")
+
+#union all 3 data sources
+conform_orders=sr_orders.union(vander_orders).union(fourth_orders)
+conform_orderdetails=sr_orderdetails.union(vander_orderdetails).union(fourth_orderdetails)
+
+#write out conformed data
+conform_orders.write.saveAsTable("c_orders", format='parquet', mode='overwrite', path='/mnt/conformed/orders' )
+conform_orderdetails.write.saveAsTable("c_orderdetails", format='parquet', mode='overwrite', path='/mnt/conformed/orderdetails' )
+
+
+# COMMAND ----------
+
+# MAGIC %sql select * from c_orders o
+# MAGIC inner join c_customers c on o.CustomerID = c.CustomerID
+
+# COMMAND ----------
+
