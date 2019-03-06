d4d71e62a181de0b6c543371c492a94ea4567428
Ryan CrawCour <38923605+ryancrawcour@users.noreply.github.com>
2019-03-06 10:47:24 -0800

    loaded csv

--- notebooks/conformation_v2.py
+++ notebooks/conformation_v2.py
@@ -4,7 +4,7 @@ configs = {"fs.azure.account.auth.type": "OAuth",
            "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
            "fs.azure.account.oauth2.client.id": "eb0cd891-c710-40b6-9059-65b226ab3998",
            "fs.azure.account.oauth2.client.secret": dbutils.secrets.get(scope = "t4adbscope", key = "aad-adls-sp"),
-           "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/OTAPRD549ops.onmicrosoft.com/oauth2/token"}
+           "fs.azure.account.oauth2.client.endpoint":                       "https://login.microsoftonline.com/OTAPRD549ops.onmicrosoft.com/oauth2/token"}
 
 # Optionally, you can add <your-directory-name> to the source URI of your mount point.
 dbutils.fs.mount(
@@ -22,3 +22,8 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
+# MAGIC %fs ls /mnt/conformed
+
+# COMMAND ----------
+
+orders = spark.read.format("csv").options(inferSchema='true', header='true').load("/mnt/raw/orders")
